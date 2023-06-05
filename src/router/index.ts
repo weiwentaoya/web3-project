504397bdd9ba24b5a36eedf4dc2c6eaf1c85b802
wentao.wei <wentao.wei@asiainnovations.com>
2023-06-05 13:36:45 +0800

    🔧 build:

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import routes from './routes'
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  // history: createWebHistory('/'),
+  history: createWebHashHistory('/'),
   routes,
   scrollBehavior() {
     // 始终滚动到顶部
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     createSvgIconsPlugin({
